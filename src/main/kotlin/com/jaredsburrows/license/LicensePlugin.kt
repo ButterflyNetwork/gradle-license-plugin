a35463de568ec5ff1419fd99d2d5b3c8147054d0
carloscoria-bfly <jcoria@butterflynetinc.com>
2026-02-19 10:52:21 -0600

    MERC-5432 Migrate to Gradle 9 and Groovy 4 compatibility (#4)
    * Migrate to Gradle 9 and Groovy 4 compatibility

--- src/main/kotlin/com/jaredsburrows/license/LicensePlugin.kt
+++ src/main/kotlin/com/jaredsburrows/license/LicensePlugin.kt
@@ -1,53 +1,34 @@
 package com.jaredsburrows.license
 
-import com.android.build.gradle.AppExtension
+import com.android.build.api.variant.AndroidComponentsExtension
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.BaseExtension
-import com.android.build.gradle.FeatureExtension
-import com.android.build.gradle.FeaturePlugin
-import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
-import com.android.build.gradle.api.BaseVariant
-import org.gradle.api.DomainObjectSet
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.api.plugins.JavaPlugin
 import java.io.File
-import kotlin.reflect.KClass
 
 /** A [Plugin] which grabs the POM.xml files from maven dependencies. */
 class LicensePlugin : Plugin<Project> {
   override fun apply(project: Project) {
     val extension = project.extensions.create("licenseReport", LicenseReportExtension::class.java)
 
-    project.plugins.all {
-      when (it) {
-        is JavaPlugin -> configureJavaProject(project, extension)
-        is FeaturePlugin -> {
-          project.extensions[FeatureExtension::class].run {
-            configureAndroidProject(project, extension, featureVariants)
-            configureAndroidProject(project, extension, libraryVariants)
-          }
-        }
-        is LibraryPlugin -> {
-          project.extensions[LibraryExtension::class].run {
-            configureAndroidProject(project, extension, libraryVariants)
-          }
-        }
-        is AppPlugin -> {
-          project.extensions[AppExtension::class].run {
-            configureAndroidProject(project, extension, applicationVariants)
-          }
-        }
-      }
+    project.plugins.withType(JavaPlugin::class.java) {
+      configureJavaProject(project, extension)
+    }
+    project.plugins.withType(LibraryPlugin::class.java) {
+      configureAndroidProject(project, extension)
+    }
+    project.plugins.withType(AppPlugin::class.java) {
+      configureAndroidProject(project, extension)
     }
   }
 
   /** Configure for Java projects. */
   private fun configureJavaProject(project: Project, extension: LicenseReportExtension) {
     val taskName = "licenseReport"
-    val path = "${project.buildDir}/reports/licenses/$taskName".replace('/', File.separatorChar)
+    val path = "${project.layout.buildDirectory.get().asFile}/reports/licenses/$taskName".replace('/', File.separatorChar)
 
     // Create tasks
     project.tasks.create(taskName, LicenseReportTask::class.java).apply {
@@ -70,16 +51,15 @@ class LicensePlugin : Plugin<Project> {
   private fun configureAndroidProject(
     project: Project,
     extension: LicenseReportExtension,
-    variants: DomainObjectSet<out BaseVariant>? = null
   ) {
-    // Configure tasks for all variants
-    variants?.all { variant ->
-      val name = variant.name.capitalize()
+    val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
+    androidComponents.onVariants { variant ->
+      val name = variant.name.replaceFirstChar { it.titlecase() }
       val taskName = "license${name}Report"
-      val path = "${project.buildDir}/reports/licenses/$taskName".replace('/', File.separatorChar)
+      val path = "${project.layout.buildDirectory.get().asFile}/reports/licenses/$taskName".replace('/', File.separatorChar)
 
       // Create tasks based on variant
-      project.tasks.create(taskName, LicenseReportTask::class.java).apply {
+      project.tasks.register(taskName, LicenseReportTask::class.java).configure {
         description = "Outputs licenses report for $name variant."
         group = "Reporting"
         csvFile = File(path + LicenseReportTask.CSV_EXT)
@@ -92,6 +72,7 @@ class LicensePlugin : Plugin<Project> {
         copyHtmlReportToAssets = extension.copyHtmlReportToAssets
         copyJsonReportToAssets = extension.copyJsonReportToAssets
         explicitDependencies = extension.explicitDependencies
+        @Suppress("DEPRECATION")
         assetDirs = (
           project
             .extensions
@@ -102,14 +83,9 @@ class LicensePlugin : Plugin<Project> {
           .assets
           .srcDirs
           .toList()
-        buildType = variant.buildType.name
+        buildType = variant.buildType
         variantName = variant.name
-        productFlavors = variant.productFlavors
       }
     }
-  }
-
-  private operator fun <T : Any> ExtensionContainer.get(type: KClass<T>): T {
-    return getByType(type.java)
   }
 }
--- src/main/kotlin/com/jaredsburrows/license/LicenseReportTask.kt
+++ src/main/kotlin/com/jaredsburrows/license/LicenseReportTask.kt
@@ -1,6 +1,5 @@
 package com.jaredsburrows.license
 
-import com.android.builder.model.ProductFlavor
 import com.jaredsburrows.license.internal.ConsoleRenderer
 import com.jaredsburrows.license.internal.pom.Developer
 import com.jaredsburrows.license.internal.pom.License
@@ -10,8 +9,8 @@ import com.jaredsburrows.license.internal.report.HtmlReport
 import com.jaredsburrows.license.internal.report.JsonReport
 import groovy.util.Node
 import groovy.util.NodeList
-import groovy.util.XmlParser
-import groovy.xml.QName
+import groovy.xml.XmlParser
+import groovy.namespace.QName
 import org.gradle.api.DefaultTask
 import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
@@ -49,7 +48,6 @@ open class LicenseReportTask : DefaultTask() { // tasks can't be final
 
   @Optional @Input
   var variantName: String? = null
-  @Internal var productFlavors = listOf<ProductFlavor>()
   @OutputFile lateinit var csvFile: File
   @OutputFile lateinit var htmlFile: File
   @OutputFile lateinit var jsonFile: File
@@ -110,9 +108,9 @@ open class LicenseReportTask : DefaultTask() { // tasks can't be final
       .forEach { artifact ->
         val id = artifact.moduleVersion.id
         val gav = "${id.group}:${id.name}:${id.version}@pom"
-        configurations.getByName(pomConfiguration).dependencies.add(
-          project.dependencies.add(pomConfiguration, gav)
-        )
+        project.dependencies.add(pomConfiguration, gav)?.let { dep ->
+          configurations.getByName(pomConfiguration).dependencies.add(dep)
+        }
       }
   }
 
@@ -163,9 +161,9 @@ open class LicenseReportTask : DefaultTask() { // tasks can't be final
         getResolvedArtifactsFromResolvedDependencies(allDeps).forEach { artifact ->
           val id = artifact.moduleVersion.id
           val gav = "${id.group}:${id.name}:${id.version}@pom"
-          configurations.getByName(pomConfiguration).dependencies.add(
-            project.dependencies.add(pomConfiguration, gav)
-          )
+          project.dependencies.add(pomConfiguration, gav)?.let { dep ->
+            configurations.getByName(pomConfiguration).dependencies.add(dep)
+          }
         }
       }
     }
@@ -254,7 +252,7 @@ open class LicenseReportTask : DefaultTask() { // tasks can't be final
       }
 
     // Sort POM information by name
-    projects.sortBy { it.name.toLowerCase() }
+    projects.sortBy { it.name.lowercase() }
   }
 
   /** Setup configurations to collect dependencies. */
@@ -287,9 +285,9 @@ open class LicenseReportTask : DefaultTask() { // tasks can't be final
     // Add dependency to temporary configuration
     val configurations = project.configurations
     configurations.create(tempPomConfiguration)
-    configurations.getByName(tempPomConfiguration).dependencies.add(
-      project.dependencies.add(tempPomConfiguration, dependency)
-    )
+    project.dependencies.add(tempPomConfiguration, dependency)?.let { dep ->
+      configurations.getByName(tempPomConfiguration).dependencies.add(dep)
+    }
 
     val pomFile = project.configurations.getByName(tempPomConfiguration)
       .resolvedConfiguration.lenientConfiguration.artifacts.firstOrNull()?.file
